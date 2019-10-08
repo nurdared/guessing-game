dd537b94aed2dc40873c9e2c91afb6992af9ca40
nurdared <nurda9727@gmail.com>
2019-10-08 22:59:14 +0800

    feat: add solution for guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.left = 0;
+        this.right = 0;
+        this.m = 0;
+    }
 
     setRange(min, max) {
-
+        this.left = min;
+        this.right = max;
     }
 
     guess() {
-
+        this.m = Math.round((this.left + this.right) / 2);
+        return this.m;
     }
 
     lower() {
-
+        this.right = this.m;    //to look left side
     }
-
+    
     greater() {
-
+        this.left = this.m;     //to look right side
     }
 }
 
